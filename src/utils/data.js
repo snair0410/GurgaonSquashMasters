e309176ad4a3f16a28c405936b904acb19fe5bdb
Sandeep Nair <sandeepnair@Sandeeps-MacBook-Air.local>
2022-06-13 00:44:54 +0530

    fifth commit

---
+++ src/utils/data.js
@@ -0,0 +1,30 @@
+export const categories = [
+    {
+    name: 'GSM Edition 1',
+    image: 'https://i.pinimg.com/236x/2e/63/c8/2e63c82dfd49aca8dccf9de3f57e8588.jpg',
+    },
+    {
+      name: 'GSM Edition 2',
+      image: 'https://i.pinimg.com/236x/2e/63/c8/2e63c82dfd49aca8dccf9de3f57e8588.jpg',
+    },
+    {
+      name: 'GSM Edition 3',
+      image: 'https://i.pinimg.com/236x/2e/63/c8/2e63c82dfd49aca8dccf9de3f57e8588.jpg',
+    },
+    {
+      name: 'GSM Edition 4',
+      image: 'https://i.pinimg.com/236x/2e/63/c8/2e63c82dfd49aca8dccf9de3f57e8588.jpg',
+    },
+    {
+      name: 'GSM Edition 5',
+      image: 'https://i.pinimg.com/236x/2e/63/c8/2e63c82dfd49aca8dccf9de3f57e8588.jpg',
+    },
+    {
+      name: 'Challenge Match',
+      image: 'https://i.pinimg.com/236x/2e/63/c8/2e63c82dfd49aca8dccf9de3f57e8588.jpg',
+    },
+    {
+        name: 'others',
+        image: 'https://i.pinimg.com/236x/2e/63/c8/2e63c82dfd49aca8dccf9de3f57e8588.jpg',
+    },
+  ];
